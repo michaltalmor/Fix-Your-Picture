0b1bf66d9bbeeca53765ef706442109afa977dda
roikatz16 <kroi@post.bgu.ac.il>
2020-12-27 11:00:09 +0200

    pretty GUI 4

--- frontend.py
+++ frontend.py
@@ -5,13 +5,9 @@ import kivy
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
-from kivy.uix.image import Image
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
 from kivymd.uix.behaviors import HoverBehavior
 
 from yolo_object_detection import Detection
@@ -34,8 +30,9 @@ class MyLayout(Widget):
     def change_picture(self, img_path="default.png"):
         self.my_image.source = img_path
         self.my_image.reload()
-        if img_path=="default.png":
+        if img_path == "default.png":
             self.detect.disabled = True
+            self.output.text = "\n Load your image to get grade"
 
     def detect_objects(self):
         self.my_detection.load_image(self.my_image.source)
@@ -44,16 +41,12 @@ class MyLayout(Widget):
         self.output.text = f"\nYour image grade is: {grade}"
         # binding function
         self.load_detected_image(img)
-        # cv2.imshow("Image", img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
     def load_detected_image(self, img):
         dot_location = self.my_image.source.rfind('.')
         new_img_path = self.my_image.source[:dot_location] + "2" + self.my_image.source[dot_location:]
         cv2.imwrite(new_img_path, img)
         self.change_picture(new_img_path)
-
 
     def load_picture(self):
         self.the_popup = FileChoosePopup(load=self.load)
@@ -72,7 +65,6 @@ class MyLayout(Widget):
         cv2.imshow("Image", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
 
 
 class MyButton(HoverBehavior, Button):
